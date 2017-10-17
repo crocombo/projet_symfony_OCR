f4fd3409cac05bf9740f472131df64cc78a8876e
crocombo <crocombo@gmail>
2017-10-17 17:30:12 +0200

    Creation BDD via orm doctrine de la table Advert(avec ces attributs)
    Modification d'1 entité av ajout d'attribut $published puis on la complette autommatiquement av doctrine:generate:entities
    
    Envoie en BDD av Doctrine
    
    Moddification addAction de AdvertController pour integrer directement en BDD
    
    Ajout d'un constructeur a la class Advert date suite a probleme de date "null" :
        // PERMET DE CREER LA DATE DIRECTEMENT DEPUIS LE CONSTRUCTEUR
        public function __construct()
            {
            // Par défaut, la date de l'annonce est la date d'aujourd'hui
            $this->date = new \Datetime();
            }
     ________________________________________________________________________________________
    
    il a fallut a# Veuillez saisir le message de validation pour vos modifications. Les lignes

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -4,6 +4,7 @@
 namespace OC\PlatformBundle\Controller;                             // Appartient a l'environement "OCPlatformBundle" (sont namesapce).
 
 
+use OC\PlatformBundle\Entity\Advert;                                // Permer de lier les entités Advert.
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;           // Notre contrôleur hérite du contrôleur de base de Symfony.
 use Symfony\Component\HttpFoundation\Request;                       // Pour récupérer la requête depuis un contrôleur.
 use Symfony\Component\HttpFoundation\Response;                      // Pour retourner une réponse depuis un contrôleur.
@@ -115,18 +116,34 @@ class AdvertController extends Controller
 
 
 
-        // La gestion d'un formulaire est particulière, mais l'idée est la suivante :
-        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
-        if ($request->isMethod('POST')) {
 
-          // Ici, on s'occupera de la création et de la gestion du formulaire
+        // Création de l'entité
+        $advert = new Advert();
+        $advert->setTitle('Recherche développeur Symfony.');
+        $advert->setAuthor('Alexandre');
+        $advert->setContent("Nous recherchons un développeur Symfony débutant sur Lyon. Blabla…");
+        // On peut ne pas définir ni la date ni la publication,
+        // car ces attributs sont définis automatiquement dans le constructeur
+
+        // On récupère l'EntityManager
+        $em = $this->getDoctrine()->getManager();
+
+        // Étape 1 : On « persiste » l'entité
+        $em->persist($advert);
+
+        // Étape 2 : On « flush » tout ce qui a été persisté avant
+        $em->flush();
+
+        // Reste de la méthode qu'on avait déjà écrit
+        if ($request->isMethod('POST')) {
           $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
 
           // Puis on redirige vers la page de visualisation de cettte annonce
-          return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+          return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
         }
+
         // Si on n'est pas en POST, alors on affiche le formulaire
-        return $this->render('OCPlatformBundle:Advert:add.html.twig', array('msg' => $test) );
+        return $this->render('OCPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
 
     }
 
